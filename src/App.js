faa903c0c4e03d95626bd95a0c966584a6321c37
Rishi Kumar <rishikrbxr9@gmail.com>
2024-08-10 00:44:07 +0530

    Fille & Folder structuring

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './App.css';
 import {Route,Routes} from 'react-router-dom';
 import { Box } from '@mui/material';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Footer  from './components/Footer';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-   <Box width ="400px">
+   <Box width ="400px" sx={{width:{x1:'1488px'}}} m ="auto">
    < Navbar />
     <Routes>
       <Route path = "/" element={<Home />} />
-      <Route path = "/excercise/:id" element={<ExcerciseDetail />} />
+      <Route path = "/excercise/:id" element={<ExerciseDetail />} />
     </Routes>
+    <Footer />
    </Box>
   )
 }
