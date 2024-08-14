ba3a637e31aa931f301229ba7f11556199b8d803
Rishi Kumar <rishikrbxr9@gmail.com>
2024-08-15 00:01:15 +0530

    Correction in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,8 @@ import Logo from '../assets/images/Logo.png'
 const Navbar = () => {
   return (
     <Stack>
-      <Link to="/">
+      <Link to="/" style ={
+    {textDecoration:'none' , color:'#3A1212', borderBottom:'3px solid #FF2625'}}>
       <img src = {Logo} alt= "logo" style={{
         width: '50px', height: '48px', margin:'0 20px'}} />
         </Link>
